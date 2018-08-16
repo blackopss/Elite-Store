04b741e7e72ede2ff362e95ed2acce636911e99c
blackopss <36933564+blackopss@users.noreply.github.com>
2018-08-17 02:45:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const fkkRecently = new Set();
+var Canvas = require('canvas')
 const dateFormat = require('dateformat');
 
 
